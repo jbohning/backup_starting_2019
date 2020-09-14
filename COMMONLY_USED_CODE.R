56242807bf1f353c797b629f7b69f8844d7451f4
jbohning <jsh1818@yahoo.com>
2020-09-14 08:39:41 -0500

    Add files via upload

--- COMMONLY_USED_CODE.R
+++ COMMONLY_USED_CODE.R
@@ -234,3 +234,15 @@ for (i in 1:3){
 #################################################################
 
 ALTER TABLE tablename CHANGE column_name new_Column_name new_column_type 
+
+
+#################################################################
+
+#CREATE DISTANCE CALCULATION FUNCTION
+
+#################################################################
+distance_in_miles<-function(lat1, lon1, lat2, lon2) {
+    p = pi/180
+    a = 0.5 - cos((lat2 - lat1) * p)/2 + cos(lat1 * p) * cos(lat2 * p) * (1 - cos((lon2 - lon1) * p)) / 2
+    12742 * asin(sqrt(a)) * 0.621371
+}
